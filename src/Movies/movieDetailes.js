025cbd5adfa234a6f73ded7bcff343fa2f839df6
sohailaezz <sohaila>
2022-06-21 23:46:31 +0200

    add pagination , detailes and search

--- src/Movies/movieDetailes.js
+++ src/Movies/movieDetailes.js
@@ -1,14 +1,53 @@
 import React from "react";
-import { useParams ,useLocation } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const MovieDetailes = (props) => {
-  const params=useParams();
+  const history = useHistory();
+  const redirectToMovies = () => {
+    history.push("/Movies");
+  }
+
+  const params = useParams();
+  const image="https://image.tmdb.org/t/p/w500/";
+  const [Movie, setMovie] = useState({});
+    useEffect(() => {
+        axios
+            .get(`https://api.themoviedb.org/3/movie?api_key=5a1d6c5c7bf57a5815b63fba30751750/${params.id}`)
+            .then((res) => {
+                console.log(res.data);
+                setMovie(res.data);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
+  // axiosInstance.post("url",{},{})
   return (
-    <div>
-        <h1>MovieDetailes{params.id}</h1>
-    </div>
-);
-  
+    <div className='container'>
+    <div className="col" key={Movie.id}>
+              <div className="card">
+                <img
+                  src={image+Movie.poster_path}
+                  style={{ height: "400px"  }}
+                  className="card-img-top"
+                  alt="..."/>
+              
+                  <h5 className="card-title">{Movie.title}</h5>
+                  <p className="card-text me-4"> {Movie.overview}</p>
+            
+              </div>
+           
+        
+          <div className='paginate'>
+          <button className="btn btn-primary mt-4" onClick={() => redirectToMovies()}>
+             Go To Movies
+      </button>
+      </div>
+        </div>
+      </div>
+    
+  );
 };
 
 export default MovieDetailes;
--- src/Movies/movies.js
+++ src/Movies/movies.js
@@ -4,38 +4,53 @@ import { useEffect,useState } from "react";
 import { Link } from "react-router-dom";
 const Movies = () => {
   const [Movies, setMovies] = useState([]);
+  const[page,setPage]=useState(1);
+  const[Query,setQuery]=useState("");
   const image="https://image.tmdb.org/t/p/w500/";
   useEffect(()=>{
-  axios.get("https://api.themoviedb.org/3/movie/popular?api_key=5a1d6c5c7bf57a5815b63fba30751750")
+  axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=5a1d6c5c7bf57a5815b63fba30751750&page=${page}`)
   .then((res)=>{
    console.log(res.data);
    setMovies(res.data.results);
   }).catch((err)=>{
    console.log(err);
   })
-  },[]);
+  },[page]);
   return (
     <>
-      <div className="row row-cols-1 row-cols-md-2 g-4">
+    <div className='container mt-4'>
+    <div class="input-group mb-3">
+        <input type="text" className='form-control search' placeholder="Search..."  onChange={(e)=>setQuery(e.target.value)}/>
+   </div>
 
-        {Movies.map((Movie) => {
+      <div className="row row-cols-1 row-cols-md-2 g-4" style={{  width:"100%" }}>
+        {Movies.filter(e=>e.title.toLowerCase().includes(Query))
+.map((Movie) => {
           return (
             <div className="col" key={Movie.id}>
               <div className="card">
                 <img
                   src={image+Movie.poster_path}
-                  style={{ height: "200px" }}
+                  style={{ height: "400px"  }}
                   className="card-img-top"
                   alt="..."/>
                 <div className="card-body">
                   <h5 className="card-title">{Movie.title}</h5>
-                  <p className="card-text">{Movie.overview}</p>
+                  <span className="card-text me-4"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> {Movie.release_date}</span>
+              <span className="card-text"><i class="fa fa-comments" aria-hidden="true"></i>{Movie.vote_count}</span>
+
                   <Link to={`/detailes/${Movie.id}`}>Details</Link>
                 </div>
               </div>
             </div>
           );
         })}
+      </div>
+      <div className="row paginate"> 
+      <button  onClick={()=>setPage(page-1)} className="col-2 btn btn-primary  mt-4 d-flex justify-content-center">Previous</button>
+      <span className='col-8'></span>
+      <button onClick={()=>setPage(page+1)} className="col-2 btn btn-primary  mx-auto  mt-4 d-flex justify-content-center">Next</button>
+      </div>
       </div>
     </>
 );
